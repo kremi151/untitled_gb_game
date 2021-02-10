000bc8c88320942f39c285f855c373bae1192f7a
Michel Kremer <kremi151dev@gmail.com>
2021-02-10 23:47:00 +0100

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,70 @@
+/**
+ * Untitled Game Boy game
+ * Copyright (C) 2021  kremi151 (Michel Kremer)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+#include "mountain_tiles.h"
+#include "mountain_map.h"
+
+void intr_lyc() {
+    //move_bkg(LYC_REG, 0);
+    //LYC_REG = (LYC_REG + 2) % 0x64;
+
+    if (LYC_REG == 0x00) {
+        // move_bkg
+        // move_win
+        move_bkg(10, 0);
+        LYC_REG = 0x30;
+    } else if (LYC_REG == 0x30) {
+        move_bkg(20, 0);
+        LYC_REG = 0x60;
+    } else if (LYC_REG == 0x60) {
+        move_bkg(30, 0);
+        LYC_REG = 0x00;
+    }
+}
+
+int main () {
+    set_bkg_data(0, 12, MountainTiles);
+    set_bkg_tiles(0, 0, MountainsMapWidth, MountainsMapHeight, MountainsMap);
+
+    //STAT_REG = 0x45;
+    //LYC_REG = 0x01;
+
+    //disable_interrupts();
+    //add_LCD(intr_lyc);
+    //enable_interrupts();
+
+    set_interrupts(VBL_IFLAG | LCD_IFLAG);
+
+    SHOW_BKG;
+    DISPLAY_ON;
+
+    /*int c = 0;
+    font_init();
+    while (c++ < 19) {
+        printf("Greetings planet!\n");
+    }*/
+
+
+    while (1) {
+        //c = 0;
+        wait_vbl_done();
+    }
+}
