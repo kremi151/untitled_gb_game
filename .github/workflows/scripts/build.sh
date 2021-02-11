7d49bdbb0924b0d5387754df17fc4a61f2729d79
Michel Kremer <kremi151dev@gmail.com>
2021-02-11 13:27:50 +0100

    Add GitHub workflow

---
+++ .github/workflows/scripts/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Upgrade to newer CMake"
+new_cmake_dir="$HOME/tmp_cmake"
+mkdir $new_cmake_dir
+cd $new_cmake_dir
+wget -q https://cmake.org/files/v3.19/cmake-3.19.2-Linux-x86_64.tar.gz
+tar -xzf cmake-3.19.2-Linux-x86_64.tar.gz
+export PATH="$new_cmake_dir/cmake-3.19.2-Linux-x86_64/bin:$PATH"
+
+echo "Test CMake installation"
+cd ~
+cmake --version
+
+echo "Build ROM"
+cd /github/workspace
+mkdir cmake-build
+cd cmake-build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DGBDK_ROOT=/opt/gbdk
+make
